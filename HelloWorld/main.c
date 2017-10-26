b2f44749e18d6f94a83db7e4b93c3a7cdab8b92d
Benjamin Kararic <Ben255@benjamins-mbp.lan>
2017-10-26 11:55:22 +0200

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Benjamin Kararic on 2017-08-29.
+//  Copyright © 2017 Benjamin Kararic. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
